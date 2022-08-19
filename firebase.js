b1f8624d8ebcfcae5b668bec102753cdcef79a15
Daniel Jude C <judedaniel360@gmail.com>
2022-08-19 03:10:15 +0100

    create section

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCd0Tio5P5opsnYD79BOIwMtlCCUJQxk0s",
+  authDomain: "nextjs-instagram-8a937.firebaseapp.com",
+  projectId: "nextjs-instagram-8a937",
+  storageBucket: "nextjs-instagram-8a937.appspot.com",
+  messagingSenderId: "34710008501",
+  appId: "1:34710008501:web:a20fc61cedba5fc57644fc",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
