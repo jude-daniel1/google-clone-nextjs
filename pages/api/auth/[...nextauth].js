fbb1a139625911be9ce3d231bdc908dc9f9a2ca2
Daniel Jude C <judedaniel360@gmail.com>
2022-08-28 11:04:29 +0100

    fixing all issues

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,18 +1,17 @@
 import NextAuth from "next-auth/next";
-import GoogleProvider from 'next-auth/providers/google'
-
+import GoogleProvider from "next-auth/providers/google";
 
 export default NextAuth({
-    providers : [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        }),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
 
-        
+  secret: process.env.SECRET,
 
-    ],
-    pages: {
-            signIn: "auth/signin"
-        }
-})
+  pages: {
+    signIn: "auth/signin",
+  },
+});
